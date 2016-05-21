9bee981cd2b73b0953bf58425f4bca51dee08661
slavavikharev <slavavikharev@142eb7da-3426-4e83-b16f-2be5c750212a>
2016-05-21 16:09:47 +0000

    Looping fixed

--- trunk/dns_cache/__main__.py
+++ trunk/dns_cache/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import argparse
 from select import select
@@ -6,11 +7,19 @@ from server import Server
 HOST = '127.0.0.1'
 PORT = 53
 
+RESERVE_FORWARDER = '8.8.8.8', 53
+
 parser = argparse.ArgumentParser()
 parser.add_argument("fhost", type=str, help="Forwarder host")
 parser.add_argument("fport", type=int, help="Forwarder port")
 parser.add_argument("--port", type=int, help="Listening port", default=PORT)
 args = parser.parse_args()
+
+try:
+    f_ip = socket.gethostbyname(args.fhost)
+except socket.error:
+    print('Use a correct forwarder')
+    sys.exit()
 
 s_addr = HOST, args.port
 f_addr = args.fhost, args.fport
@@ -21,4 +30,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         if not select([sock], [], [])[0]:
             continue
         data, cl_addr = sock.recvfrom(4096)
-        Server(data, cl_addr, f_addr, sock).start()
+
+        cl_ip = socket.gethostbyname(cl_addr[0])
+        forwarder = RESERVE_FORWARDER if cl_ip == f_ip else f_addr
+
+        Server(data, cl_addr, forwarder, sock).start()
